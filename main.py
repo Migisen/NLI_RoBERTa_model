4b275d242cf7f28c9a9cb163aefa106ee76d4e88
Migisen <emelds1@yandex.ru>
2021-03-12 00:31:32 +0300

    Реализовал токенизацию и обработку датасета

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from data import data_loader
-test = data_loader.SNLIData(data_name='snli_1.0_train')
+from data import snli_dataset
+from torch.utils.data import DataLoader
+
+test_data = snli_dataset.SNLIData(data_name='snli_1.0_train')
 
 
 if __name__ == '__main__':
-    print(test.__getitem__(10))
+    test_loader = DataLoader(test_data, batch_size=100)
+    print(test_data.__getitem__(10))
     print('Done!')
 
 
